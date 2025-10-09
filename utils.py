239db2f8344aedf5a4bb02ee3b587b3f30e80802
gagik-philips <partner.gagik.khalafyan@philips.com>
2025-10-09 10:19:28 +0400

    Project setup

--- utils.py
+++ utils.py
@@ -17,3 +17,4 @@ def configure_logging() -> None:
     """Configures logging"""
     logging.basicConfig(level=logging.INFO, 
                         format='%(asctime)s - %(levelname)s - %(message)s')
+    
